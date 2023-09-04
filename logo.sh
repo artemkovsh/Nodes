de763041ff68ed3142f1579bfb94a50edd960aa4
artemkovsh <85120839+artemkovsh@users.noreply.github.com>
2023-09-04 20:10:23 +0400

    logo.sh

---
+++ logo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function colors {
+  GREEN="\e[32m"
+  RED="\e[39m"
+  NORMAL="\e[0m"
+}
+
+colors
+
+echo -e "${GREEN}╱╭╮╱╱╱╱╱╭━╮╱╭╮╱╱╱╱╭╮╱╱╱╱╱${NORMAL}"
+echo -e "${GREEN}╭╯╰╮╱╱╱╱┃┃╰╮┃┃╱╱╱╱┃┃╱╱╱╱╱${NORMAL}"
+echo -e "${GREEN}╰╮╭╋━┳╮╭┫╭╮╰╯┣━━┳━╯┣━━┳━━╮${NORMAL}"
+echo -e "${GREEN}╱┃┃┃╭┫┃┃┃┃╰╮┃┃╭╮┃╭╮┃┃━┫━━┫${NORMAL}"
+echo -e "${GREEN}╱┃╰┫┃┃╰╯┃┃╱┃┃┃╰╯┃╰╯┃┃━╋━━┃${NORMAL}"
+echo -e "${GREEN}╱╰━┻╯╰━━┻╯╱╰━┻━━┻━━┻━━┻━━╯${NORMAL}"
+
+echo -e "$ Наш Телеграмм канал t.me/truNodes${NORMAL}"
+echo -e "$ Наш ЧАТ t.me/truNodesChat${NORMAL}"
