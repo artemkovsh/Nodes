228be886d9a1a69dc92adc656fa8e5ed763054e5
_Cryptique_ <85120839+artemkovsh@users.noreply.github.com>
2024-04-06 02:24:28 +0400

    Create avail.sh

---
+++ avail.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/artemkovsh/Nodes/main/logoc.sh | bash
+echo "-----------------------------------------------------------------------------"
+function update {
+    sudo apt update
+    sudo apt install mc wget curl git htop netcat net-tools unzip jq build-essential ncdu tmux make cmake clang pkg-config libssl-dev protobuf-compiler -y
+
+
+EOF
+}
+    curl -sL1 avail.sh | bash
+
+echo "-----------------install nodes-----------------"
+curl -sL1 avail.sh | bash
+echo "-----------------------------------------------------------------------------"
+
+main
